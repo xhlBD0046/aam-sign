52a9fe4381b66edd0d538e55796318dfe29f1285
Desktop 12138 <hxuu0046@gmail.com>
2025-12-07 20:38:41 -0800

    feat: Add initial Django project settings with environment variable loading, static/media configuration, and Stripe integration.

--- aamsign/aamsign/settings.py
+++ aamsign/aamsign/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
